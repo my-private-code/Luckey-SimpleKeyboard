a0dcbfb7bfa7df3997bd91332f90ac345f411f2d
Dong Yuwei <newdongyuwei@gmail.com>
2024-04-13 08:51:28 +0800

    Initial Commit

---
+++ Luckey202404/Luckey202404App.swift
@@ -0,0 +1,17 @@
+//
+//  Luckey202404App.swift
+//  Luckey202404
+//
+//  Created by Yuwei Dong on 13/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Luckey202404App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
