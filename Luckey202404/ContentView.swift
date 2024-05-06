580d5c78c8177c28d37ba63c916a8f4fef08fbdb
Dong Yuwei <newdongyuwei@gmail.com>
2024-05-06 20:47:13 +0800

    rm commented code

--- Luckey202404/ContentView.swift
+++ Luckey202404/ContentView.swift
@@ -36,55 +36,6 @@ struct ContentView: View {
     }
 }
 
-//struct ContentView: View {
-//    private var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 5)
-//    @State private var isExpanded: Bool = false
-//
-//    var body: some View {
-//        ZStack(alignment: .bottom) {
-//            mainContentView
-//            if isExpanded {
-//                expandedGridView
-//                    .animation(.easeInOut, value: isExpanded)
-//                    .transition(.move(edge: .bottom))
-//            }
-//        }
-//        .edgesIgnoringSafeArea(.all)
-//    }
-//
-//    var mainContentView: some View {
-//        Button(action: {
-//            withAnimation {
-//                isExpanded.toggle()
-//            }
-//        }) {
-//            Text(isExpanded ? "Collapse" : "Expand")
-//                .frame(maxWidth: .infinity, maxHeight: .infinity)
-//                .background(Color.blue)
-//                .foregroundColor(.white)
-//                .font(.headline)
-//        }
-//    }
-//
-//    var expandedGridView: some View {
-//        VStack {
-//            Spacer()
-//            LazyVGrid(columns: columns, spacing: 20) {
-//                ForEach(0..<10, id: \.self) { item in
-//                    Text("Item \(item)")
-//                        .frame(height: 100)
-//                        .background(Color.green)
-//                        .cornerRadius(10)
-//                }
-//            }
-//            .padding()
-//            .background(Color.black.opacity(0.5))
-//            .cornerRadius(12)
-//        }
-//        
-//    }
-//}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
